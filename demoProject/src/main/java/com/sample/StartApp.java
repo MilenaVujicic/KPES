ecf67b8d6f373614e3a57d51609d6b55910cd4d6
MAndrea98 <mendrei.andrea@gmail.com>
2020-05-10 20:58:42 +0200

    Some changes

--- demoProject/src/main/java/com/sample/StartApp.java
+++ demoProject/src/main/java/com/sample/StartApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EntityScan("com.sample.model")
 public class StartApp {
 	public static void main(String[] args) {
 		SpringApplication.run(StartApp.class, args);
--- demoProject/src/test/java/com/sample/Testiranje.java
+++ demoProject/src/test/java/com/sample/Testiranje.java
@@ -50,48 +50,6 @@ public class Testiranje {
 	public void testiranje() {
 		KieSession kSession = KnowledgeSessionHelper.getStatefulKnowledgeSession(kieContainer, "ksession-rules");
 		
-		Obelezje o1 = new Obelezje();
-		o1.setSubjektivanOdnos("nehat");
-		o1.setDelo1(new Delo());
-		Obelezje o2 = new Obelezje();
-		o2.setRadnja("tokom razbojnistva ili razbojnicke kradje");
-		o2.setZrtva(">18");
-		o2.setDelo1(new Delo());
 		
-		Delo delo1 = new Delo();
-		delo1.setMaxKazna(7);
-		
-		Delo delo2 = new Delo();
-		delo2.setMaxKazna(11);
-		
-		Delo delo3 = new Delo();
-		delo3.setMaxKazna(9.9);
-		
-		List<Obelezje> obelezija = obelezjeService.findAll();
-		System.out.println("###" + obelezija.size());
-		
-		
-		kSession.insert(o1);
-		kSession.insert(o2);
-		kSession.insert(delo1);
-		kSession.insert(delo2);
-		kSession.insert(delo3);
-		
-		int fired = kSession.fireAllRules();
-		
-		@SuppressWarnings("unchecked")
-		Collection<Tuzilac> tuz = (Collection<Tuzilac>) kSession.getObjects(new ClassObjectFilter(Tuzilac.class));
-		ArrayList<Tuzilac> tuzioci = new ArrayList<Tuzilac>();
-		for(Tuzilac t : tuz) {
-			tuzioci.add(t);
-		}
-		
-		assertEquals(fired, TEST_NUM);
-		assertEquals(o1.getDelo1().getClan(), 118);
-		assertEquals(o2.getDelo1().getClan(), 114);
-		assertEquals(o2.getDelo1().getStav(), 1);
-		assertEquals(o2.getDelo1().getTacka(), 9);
-		assertEquals(tuz.size(), 2);
-		assertEquals(tuzioci.get(0).getTip(), TipTuzioca.OSNOVNI_JAVNI_TUZILAC);
 	}
 }
