2984456c35f02867e93bd868b9c668d5ce2d98af
MAndrea98 <mendrei.andrea@gmail.com>
2020-05-12 19:25:06 +0200

    Changed version

--- demoProject/src/main/java/com/sample/StartApp.java
+++ demoProject/src/main/java/com/sample/StartApp.java
@@ -2,10 +2,8 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.sample.model")
 public class StartApp {
 	public static void main(String[] args) {
 		SpringApplication.run(StartApp.class, args);
