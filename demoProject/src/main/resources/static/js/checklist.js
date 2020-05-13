0b505fbf887b29955698f71603629f09279e5350
MilenaVujicic <44505736+MilenaVujicic@users.noreply.github.com>
2020-05-13 20:54:52 +0200

    Added query data parser

--- demoProject/src/main/resources/static/js/checklist.js
+++ demoProject/src/main/resources/static/js/checklist.js
@@ -7,12 +7,39 @@ $(document).ready(function() {
 function sendData(event){
 	event.preventDefault();
 
+	var starost1 = $('#sAge1').val();
+	var starost2 = $('#sAge2').val();
 	var subOdnos = $('#sSubOdnos').val();
+	var stanje = $('#sStanje').val();
+	var mesto = $('#sMesto').val();
+	var radnja = $('#sRadnja').val();
+	var broj = $('#sBroj').val();
 	
+	if(starost1 == null){
+		starost1 = ['nema podataka'];
+	}
+	if(starost2 == null){
+		starost2 = ['nema podataka'];
+	}
+	if(subOdnos == null){
+		subOdnos = ['nema podataka'];
+	}
+	if(stanje == null){
+		stanje = ['nema podataka'];
+	}
+	if(mesto == null){
+		mesto = ['nema podataka'];
+	}
+	if(radnja == null){
+		radnja = ['nema podataka'];
+	}
+	if(broj == null){
+		broj = ['nema podataka'];
+	}
 	$.ajax({
 		url:'data/sendData',
 		type: 'POST',
-		data: JSON.stringify({subOdnos : subOdnos}),
+		data: JSON.stringify({starost1: starost1, starost2 : starost2, subOdnos : subOdnos, stanje: stanje, mesto: mesto, radnja: radnja, broj: broj}),
 		contentType: 'application/json',
 		success: function(){
 		},
